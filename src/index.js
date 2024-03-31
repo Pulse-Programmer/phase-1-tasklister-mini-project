d12a1b297aba415da47958ebcc4eb6c1468faac5
Stephen Ochieng <materiamystic@gmail.com>
2024-03-31 13:23:54 +0300

    Adds required functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addMyToDos(e.target.newTaskDescription.value);
+    form.reset();
+  });
+
+  const ul = document.querySelector("#tasks");
+
+  function addMyToDos(todo) {
+    const li = document.createElement("li");
+    li.textContent = `${todo}  `;
+    const btn = document.createElement("button");
+    btn.textContent = "X";
+    li.appendChild(btn);
+    ul.appendChild(li);
+
+    btn.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+  }
 });
